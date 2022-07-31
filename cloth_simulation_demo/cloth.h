dbfa1a0ac5240d53ae689d41ee4d30edec611c41
tlcui <t.l.cui@foxmail.com>
2022-07-31 22:10:59 +0800

    添加项目文件。

---
+++ cloth_simulation_demo/cloth.h
@@ -0,0 +1,376 @@
+#pragma once
+#ifndef CLOTH_H_
+#define CLOTH_H_
+
+#define EIGEN_STACK_ALLOCATION_LIMIT 0
+
+#include <iostream>
+#include <random>
+#include <vector>
+#include <cmath>
+#include <utility>
+#include <Eigen/dense>
+#include <tbb/parallel_for.h>
+
+using namespace Eigen;
+
+static constexpr int spring_Y = 1e4;
+static constexpr int dashpot_damping = 1e4;
+static constexpr int drag_damping = 1;
+static constexpr float fraction = 0.99f;
+static constexpr float pi = 3.141592653589793f;
+
+std::random_device rd;
+std::mt19937 generator(rd());
+std::uniform_real_distribution<float> dis(0., 1.0);
+
+std::vector<std::pair<int, int>> spring_offset{ {-2,0},{-1,-1},{-1,0},{-1,1},{0,-2},{0,-1},{0,1},{0,2},{1,-1},{1,0},{1,1},{2,0} };
+
+template<typename T>
+using Vector3 = Matrix<T, 3, 1>;
+
+template<typename T>
+using Vector2 = Matrix<T, 2, 1>;
+
+template<int M, int N, typename T = float>
+class Cloth
+{
+public:
+	Cloth(const T quad_size);
+	~Cloth();
+
+	void initialize();
+
+public:
+	Array<Vector3<T>, M, N> position;
+	Array<Vector3<T>, M, N> velocity;
+	T quad_size;
+};
+
+
+template<int Number, typename T = float>
+class Balls
+{
+public:
+	Balls(const T radius);
+	~Balls();
+
+	void initialize();
+
+public:
+	Array<Vector3<T>, Number, 1> center;
+	T quad_size_ball;
+	T radius;
+};
+
+template<int M, int N, typename T = float>
+class Cloth_mesh
+{
+public:
+	Cloth_mesh();
+	~Cloth_mesh();
+
+	void update_vertices(const Cloth<M, N, T>& cloth);
+
+public:
+	unsigned int* indices;
+	T* vertices;
+};
+
+template<int Number, int X_SEGMENTS = 30, int Y_SEGMENTS = 30, typename T = float>
+class Balls_mesh
+{
+public:
+	Balls_mesh();
+	~Balls_mesh();
+
+	void update_vertices(const Balls<Number, T>& balls);
+
+public:
+	unsigned int* indices;
+	T* vertices;
+};
+
+template<int M, int N, typename T>
+inline Cloth<M, N, T>::Cloth(const T quad_size)
+{
+	position = Array<Vector3<T>, M, N>::Zero();
+	velocity = Array<Vector3<T>, M, N>::Zero();
+	this->quad_size = quad_size;
+}
+
+template<int M, int N, typename T>
+inline Cloth<M, N, T>::~Cloth()
+{
+}
+
+template<int M, int N, typename T>
+inline void Cloth<M, N, T>::initialize()
+{
+	T random_offset_x = 0.1 * (dis(generator) - 0.5);
+	T random_offset_z = 0.1 * (dis(generator) - 0.5);
+
+	tbb::parallel_for(tbb::blocked_range<int>(0, N), [&](const tbb::blocked_range<int>& r)
+		{
+			for (int j = r.begin(); j != r.end(); ++j)
+			{
+				for (int i = 0; i < M; ++i)
+				{
+					position.coeffRef(i, j).coeffRef(0) = i * quad_size - 0.5 + random_offset_x;
+					position.coeffRef(i, j).coeffRef(1) = 0.6;
+					position.coeffRef(i, j).coeffRef(2) = j * quad_size - 0.5 + random_offset_z;
+					velocity(i, j) = Vector3<T>::Zero();
+				}
+			}
+		}
+	);
+}
+
+template<int Number, typename T>
+inline Balls<Number, T>::Balls(const T radius)
+{
+	this->radius = radius;
+	this->quad_size_ball = 0;
+	center = Array<Vector3<T>, Number, 1>::Zero();
+}
+
+template<int Number, typename T>
+inline Balls<Number, T>::~Balls()
+{
+}
+
+template<int Number, typename T>
+inline void Balls<Number, T>::initialize()
+{
+	quad_size_ball = 1.0 / Number;
+	tbb::parallel_for(tbb::blocked_range<int>(0, Number), [&](const tbb::blocked_range<int>& r)
+		{
+			for (int i = r.begin(); i != r.end(); ++i)
+			{
+				center.coeffRef(i).coeffRef(0) = (i * quad_size_ball - 0.4 + (dis(generator) - 0.5) / 15) * 0.9;
+				center.coeffRef(i).coeffRef(1) = ((dis(generator) - 0.5) / 3 - 0.1) * 0.9;
+				center.coeffRef(i).coeffRef(2) = (i * quad_size_ball - 0.4 + (dis(generator) - 0.5) / 15) * 0.9;
+			}
+		}
+	);
+}
+
+
+template<int M, int N, int Number, typename T=float>
+void substep(Cloth<M, N, T>& cloth, const Balls<Number, T>& balls, const T dt)
+{
+	tbb::parallel_for(tbb::blocked_range<int>(0, N), [&](const tbb::blocked_range<int>& r)
+		{
+			for (int j = r.begin(); j != r.end(); ++j)
+			{
+				for (int i = 0; i < M; ++i)
+				{
+					Vector3<T> force(0., -9.8, 0.); //gravity
+					for (auto& offset : spring_offset)
+					{
+						auto& [offset_i, offset_j] = offset;
+						int another_i = i + offset_i;
+						int another_j = j + offset_j;
+						if (another_i >= 0 && another_i < M && another_j >= 0 && another_j < N)
+						{
+							Vector3<T> x_diff(cloth.position.coeff(i, j) - cloth.position.coeff(another_i, another_j));
+							Vector3<T> v_diff(cloth.velocity.coeff(i, j) - cloth.velocity.coeff(another_i, another_j));
+							T current_dist = x_diff.norm();
+							Vector3<T> d(x_diff / current_dist);
+							T original_dist = cloth.quad_size * (Vector2<T>(-offset_i, -offset_j).norm());
+
+							force += (-spring_Y * d * (current_dist / original_dist - 1)); //spring force
+							force += (-v_diff.dot(d) * d * dashpot_damping * cloth.quad_size); //dashpot damping
+						}
+					}
+
+					cloth.velocity.coeffRef(i, j) += (force * dt);
+				}
+			}
+		}
+	);
+
+	tbb::parallel_for(tbb::blocked_range<int>(0, N), [&](const tbb::blocked_range<int>& r)
+		{
+			for (int j = r.begin(); j != r.end(); ++j)
+			{
+				for (int i = 0; i < M; ++i)
+				{
+					cloth.velocity.coeffRef(i, j) *= std::expf(-drag_damping * dt);
+					for (int k = 0; k < Number; ++k)  //handling collision with balls
+					{
+						Vector3<T> offset_to_center(cloth.position.coeff(i, j) - balls.center.coeff(k));
+						if (offset_to_center.norm() <= balls.radius)
+						{
+							Vector3<T> normal(offset_to_center.normalized());
+							cloth.velocity.coeffRef(i, j) -= (std::min(cloth.velocity.coeff(i, j).dot(normal), static_cast<T>(0)) * normal);
+							cloth.velocity.coeffRef(i, j) *= fraction;
+						}
+					}
+
+					cloth.position.coeffRef(i, j) += (cloth.velocity.coeffRef(i, j) * dt);
+				}
+			}
+		}
+	);
+}
+
+
+template<int M, int N, typename T>
+inline Cloth_mesh<M, N, T>::Cloth_mesh()
+{
+	int triangle_number = (M - 1) * (N - 1) * 2;
+	indices = new unsigned int[triangle_number * 3];
+	vertices = new T[M * N * 6];
+	tbb::parallel_for(tbb::blocked_range<int>(0, M-1), [&](const tbb::blocked_range<int>& r)
+		{
+			for (int i = r.begin(); i != r.end(); ++i)
+			{
+				for (int j = 0; j != N - 1; ++j)
+				{
+					int square_index = i * (N - 1) + j;
+					int index = 6 * square_index;
+
+					//first triangle
+					indices[index + 0] = (i * N + j);
+					indices[index + 1] = ((i + 1) * N + j);
+					indices[index + 2] = (i * N + j + 1);
+
+					//second triangle
+					indices[index + 3] = ((i + 1) * N + j + 1);
+					indices[index + 4] = (i * N + j + 1);
+					indices[index + 5] = ((i + 1) * N + j);
+				}
+			}
+		}
+	);
+
+	tbb::parallel_for(tbb::blocked_range<int>(0, N), [&](const tbb::blocked_range<int>& r)
+		{
+			for (int j = r.begin(); j != r.end(); ++j)
+			{
+				for (int i = 0; i < M; ++i)
+				{
+					int index = 6 * (i * N + j);
+					if ((i / 4 + j / 4) % 2 == 0)
+					{
+						vertices[index + 3] = 0.0;
+						vertices[index + 4] = 0.5;
+						vertices[index + 5] = 1.0;
+					}
+					else
+					{
+						vertices[index + 3] = 1.0;
+						vertices[index + 4] = 0.5;
+						vertices[index + 5] = 0.0;
+					}
+				}
+			}
+		}
+	);
+}
+
+template<int M, int N, typename T>
+inline Cloth_mesh<M, N, T>::~Cloth_mesh()
+{
+	delete [] indices;
+	delete [] vertices;
+}
+
+template<int M, int N, typename T>
+inline void Cloth_mesh<M, N, T>::update_vertices(const Cloth<M, N, T>& cloth)
+{
+	tbb::parallel_for(tbb::blocked_range<int>(0, N), [&](const tbb::blocked_range<int>& r)
+		{
+			for (int j = r.begin(); j != r.end(); ++j)
+			{
+				for (int i = 0; i < M; ++i)
+				{
+					int index = 6 * (i * N + j);
+					vertices[index + 0] = cloth.position.coeff(i, j).x();
+					vertices[index + 1] = cloth.position.coeff(i, j).y();
+					vertices[index + 2] = cloth.position.coeff(i, j).z();
+				}
+			}
+		}
+	);
+}
+
+template<int Number, int X_SEGMENTS, int Y_SEGMENTS, typename T>
+inline Balls_mesh<Number, X_SEGMENTS, Y_SEGMENTS, T>::Balls_mesh()
+{
+	vertices = new T[Number * (X_SEGMENTS+1) * (Y_SEGMENTS+1) * 6]; // position and normal vector
+	indices = new unsigned int[Number * X_SEGMENTS * Y_SEGMENTS * 6];
+
+	for (int number = 0; number < Number; ++number)
+	{
+		int ball_index = number * X_SEGMENTS * Y_SEGMENTS * 6;
+		int ball_index_of_vertices = number * (X_SEGMENTS + 1) * (Y_SEGMENTS + 1);
+		tbb::parallel_for(tbb::blocked_range<int>(0, X_SEGMENTS), [&](const tbb::blocked_range<int>& r)
+			{
+				for (int i = r.begin(); i != r.end(); ++i)
+				{
+					for (int j = 0; j < Y_SEGMENTS; ++j)
+					{
+						int square_index = i * Y_SEGMENTS + j;
+						int index = square_index * 6;
+						indices[ball_index + index + 0] = ball_index_of_vertices + i * (Y_SEGMENTS + 1) + j;
+						indices[ball_index + index + 1] = ball_index_of_vertices + (i + 1) * (Y_SEGMENTS + 1) + j;
+						indices[ball_index + index + 2] = ball_index_of_vertices + i * (Y_SEGMENTS + 1) + j + 1;
+						indices[ball_index + index + 3] = ball_index_of_vertices + (i + 1) * (Y_SEGMENTS + 1) + j + 1;
+						indices[ball_index + index + 4] = ball_index_of_vertices + i * (Y_SEGMENTS + 1) + j + 1;
+						indices[ball_index + index + 5] = ball_index_of_vertices + (i + 1) * (Y_SEGMENTS + 1) + j;
+					}
+				}
+			}
+		);
+	}
+}
+
+template<int Number, int X_SEGMENTS, int Y_SEGMENTS, typename T>
+inline Balls_mesh<Number, X_SEGMENTS, Y_SEGMENTS, T>::~Balls_mesh()
+{
+	delete [] indices;
+	delete [] vertices;
+}
+
+template<int Number, int X_SEGMENTS, int Y_SEGMENTS, typename T>
+inline void Balls_mesh<Number, X_SEGMENTS, Y_SEGMENTS, T>::update_vertices(const Balls<Number, T>& balls)
+{
+	T radius = balls.radius * 0.95;
+	for (int number = 0; number < Number; ++number)
+	{
+		int ball_index_of_vertices = number * (X_SEGMENTS + 1) * (Y_SEGMENTS + 1) * 6;
+		T center_x = balls.center.coeff(number).x();
+		T center_y = balls.center.coeff(number).y();
+		T center_z = balls.center.coeff(number).z();
+		tbb::parallel_for(tbb::blocked_range<int>(0, X_SEGMENTS + 1), [&](const tbb::blocked_range<int>& r)
+			{
+				for (int i = r.begin(); i != r.end(); ++i)
+				{
+					T x_seg = static_cast<T>(i) / static_cast<T>(X_SEGMENTS);
+					T cos_fi = std::cos(x_seg * 2 * pi);
+					T sin_fi = std::sin(x_seg * 2 * pi);
+					for (int j = 0; j < Y_SEGMENTS + 1; ++j)
+					{
+						T y_seg = static_cast<T>(j) / static_cast<T>(Y_SEGMENTS);
+						int index = 6 * (i * (Y_SEGMENTS+1) + j);
+						T sin_theta = std::sin(y_seg * pi);
+						T cos_theta = std::cos(y_seg * pi);
+						T normal_vec_x = sin_theta * cos_fi;
+						T normal_vec_y = cos_theta;
+						T normal_vec_z = sin_theta * sin_fi;
+						vertices[ball_index_of_vertices + index + 0] = center_x + radius * normal_vec_x;
+						vertices[ball_index_of_vertices + index + 1] = center_y + radius * normal_vec_y;
+						vertices[ball_index_of_vertices + index + 2] = center_z + radius * normal_vec_z;
+						vertices[ball_index_of_vertices + index + 3] = normal_vec_x;
+						vertices[ball_index_of_vertices + index + 4] = normal_vec_y;
+						vertices[ball_index_of_vertices + index + 5] = normal_vec_z;
+					}
+				}
+			}
+		);
+	}
+}
+
+#endif
---
+++ cloth_simulation_demo/main.cpp
@@ -0,0 +1,363 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include "cloth.h"
+
+#include <iostream>
+
+#define ASSERT(x) if(!(x)) __debugbreak();
+#define GLCALL(x) gl_clear_error();\
+    x;\
+    ASSERT(gl_log_error(#x, __FILE__, __LINE__))
+
+static void gl_clear_error()
+{
+    while (glGetError() != GL_NO_ERROR);
+}
+
+static bool gl_log_error(const char* function, const char* file, int line)
+{
+    while (GLenum errorcode = glGetError())
+    {
+        std::string error("");
+        switch (errorcode)
+        {
+        case GL_INVALID_ENUM:                  error = "INVALID_ENUM"; break;
+        case GL_INVALID_VALUE:                 error = "INVALID_VALUE"; break;
+        case GL_INVALID_OPERATION:             error = "INVALID_OPERATION"; break;
+        //case GL_STACK_OVERFLOW:                error = "STACK_OVERFLOW"; break;
+        //case GL_STACK_UNDERFLOW:               error = "STACK_UNDERFLOW"; break;
+        case GL_OUT_OF_MEMORY:                 error = "OUT_OF_MEMORY"; break;
+        case GL_INVALID_FRAMEBUFFER_OPERATION: error = "INVALID_FRAMEBUFFER_OPERATION"; break;
+        default:break;
+        }
+        std::cout << "opengl error (" << errorcode << "): " << error << ", " << function << ", " << file << ": " << line << std::endl;
+        return false;
+    }
+    return true;
+}
+//code above is used for debugging
+
+static constexpr int n = 128;
+static constexpr float quad_size = 1.0 / n;
+static constexpr float dt = 4e-2 / n;
+static constexpr int substeps = static_cast<int>(1.0 / 60 / dt);
+
+static constexpr int ball_number = 5;
+static constexpr float ball_radius = 0.5 / ball_number;
+static constexpr int ball_mesh_resolution_x = 40;
+static constexpr int ball_mesh_resolution_y = 40;
+
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
+"uniform mat4 model;\n"
+"uniform mat4 view;\n"
+"uniform mat4 projection;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = projection * view * model * vec4(aPos, 1.0f);\n"
+"   ourColor = aColor;\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"in vec3 ourColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(ourColor, 1.0f);\n"
+"}\n\0";
+
+const char* ball_vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aNormal;\n"
+"uniform mat4 model;\n"
+"uniform mat4 view;\n"
+"uniform mat4 projection;\n"
+"out vec3 Normal;\n"
+"out vec3 FragPos;\n"
+"void main()\n"
+"{\n"
+"   FragPos = vec3(model * vec4(aPos, 1.0f));\n"
+"   Normal = mat3(transpose(inverse(model))) * aNormal;\n"
+"   gl_Position = projection * view * model * vec4(aPos, 1.0f);\n"
+"}\0";
+
+const char* ball_fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"in vec3 Normal;\n"
+"in vec3 FragPos;\n"
+"uniform vec3 lightPos;\n"
+"uniform vec3 viewPos;\n"
+"uniform vec3 objectColor;\n"
+"uniform vec3 lightColor;\n"
+"void main()\n"
+"{\n"
+"    float ambientStrength = 0.1;\n"
+"    vec3 ambient = ambientStrength * lightColor;\n"
+"    vec3 norm = normalize(Normal);\n"
+"    vec3 lightDir = normalize(lightPos - FragPos);\n"
+"    float diff = max(dot(norm, lightDir), 0.0);\n"
+"    vec3 diffuse = diff * lightColor;\n"
+"    float specularStrength = 0.5;\n"
+"    vec3 viewDir = normalize(viewPos - FragPos);\n"
+"    vec3 reflectDir = reflect(-lightDir, norm);\n"
+"    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);\n"
+"    vec3 specular = specularStrength * spec * lightColor;\n"
+"    vec3 result = (ambient + diffuse + specular) * objectColor;\n"
+"    FragColor = vec4(result, 1.0);\n"
+"}\n\0";
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+// settings
+static constexpr unsigned int SCR_WIDTH = 1024;
+static constexpr unsigned int SCR_HEIGHT = 1024;
+
+// build and compile our shader program
+unsigned int generate_shader(const char* vertexShaderSource, const char* fragmentShaderSource)
+{
+    // vertex shader
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // check for shader compile errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // fragment shader
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // check for shader compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // link shaders
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    // check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return shaderProgram;
+}
+
+int main()
+{
+    Cloth<n, n> cloth(quad_size);
+    cloth.initialize();
+
+    Balls<ball_number> balls(ball_radius);
+    balls.initialize();
+
+    Cloth_mesh<n, n> mesh;
+    mesh.update_vertices(cloth);
+
+    Balls_mesh<ball_number,ball_mesh_resolution_x, ball_mesh_resolution_y> balls_mesh;
+    balls_mesh.update_vertices(balls);
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // glfw window creation
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "C++ cloth simulation", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // glad: load all OpenGL function pointers
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    
+    unsigned int shaderProgram = generate_shader(vertexShaderSource, fragmentShaderSource);
+    unsigned int ball_shaderProgram = generate_shader(ball_vertexShaderSource, ball_fragmentShaderSource);
+
+    // ---for cloth_mesh---
+    unsigned int VBO, VAO, EBO; 
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float)*6*n*n, mesh.vertices, GL_STREAM_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int)*6*(n-1)*(n-1), mesh.indices, GL_STREAM_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    // ---for balls_mesh---
+    unsigned int VBO_balls, VAO_balls, EBO_balls;
+    glGenVertexArrays(1, &VAO_balls);
+    glGenBuffers(1, &VBO_balls);
+    glGenBuffers(1, &EBO_balls);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(VAO_balls);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_balls);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * ball_number * (ball_mesh_resolution_x+1) * (ball_mesh_resolution_y+1) * 6, balls_mesh.vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO_balls);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * ball_number * 6 * ball_mesh_resolution_x* ball_mesh_resolution_y, balls_mesh.indices, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // normal vector attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    float current_t = 0.f;
+
+    double last_time = glfwGetTime();;
+    // render loop
+    glEnable(GL_DEPTH_TEST);
+    while (!glfwWindowShouldClose(window))
+    {
+        // show fps
+        double current_time = glfwGetTime();
+        double fps = 1 / (current_time - last_time);
+        std::stringstream ss;
+        ss << "C++ cloth simulation " <<  fps << " FPS";
+        glfwSetWindowTitle(window, ss.str().c_str());
+        last_time = current_time;
+
+        // input
+        processInput(window);
+        
+        glClearColor(0.f, 0.f, 0.f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        if (current_t > 1.5)
+        {
+            cloth.initialize();
+            balls.initialize();
+            balls_mesh.update_vertices(balls);
+            glBindVertexArray(VAO_balls);
+            glBindBuffer(GL_ARRAY_BUFFER, VBO_balls);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(float) * ball_number * (ball_mesh_resolution_x + 1) * (ball_mesh_resolution_y + 1) * 6, balls_mesh.vertices, GL_STATIC_DRAW);
+            current_t = 0.f;
+        }
+
+        for (int i = 0; i < substeps; ++i)
+        {
+            substep(cloth, balls, dt);
+            current_t += dt;
+        }
+
+        mesh.update_vertices(cloth);
+        
+        // ---render cloth---
+        glUseProgram(shaderProgram);
+
+        glm::mat4 model = glm::mat4(1.0f);
+        glm::mat4 view = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+        glm::mat4 projection = glm::mat4(1.0f);
+        projection = glm::perspective(glm::radians(45.0f), static_cast<float>(SCR_WIDTH) / static_cast<float>(SCR_HEIGHT), 0.1f, 100.0f);
+        view = glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f),  
+            glm::vec3(0.0f, 0.0f, 0.0f),           
+            glm::vec3(0.0f, 1.0f, 0.0f));
+
+        int model_loc = glGetUniformLocation(shaderProgram, "model");
+        glUniformMatrix4fv(model_loc, 1, GL_FALSE, glm::value_ptr(model));
+        int view_loc = glGetUniformLocation(shaderProgram, "view");
+        glUniformMatrix4fv(view_loc, 1, GL_FALSE, glm::value_ptr(view));
+        int project_loc = glGetUniformLocation(shaderProgram, "projection");
+        glUniformMatrix4fv(project_loc, 1, GL_FALSE, glm::value_ptr(projection));
+
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * n * n, mesh.vertices, GL_STREAM_DRAW);
+
+        glDrawElements(GL_TRIANGLES, (n-1)*(n-1)*6, GL_UNSIGNED_INT, 0);
+
+        // ---render balls---
+        glUseProgram(ball_shaderProgram);
+
+        model_loc = glGetUniformLocation(ball_shaderProgram, "model");
+        glUniformMatrix4fv(model_loc, 1, GL_FALSE, glm::value_ptr(model));
+        view_loc = glGetUniformLocation(ball_shaderProgram, "view");
+        glUniformMatrix4fv(view_loc, 1, GL_FALSE, glm::value_ptr(view));
+        project_loc = glGetUniformLocation(ball_shaderProgram, "projection");
+        glUniformMatrix4fv(project_loc, 1, GL_FALSE, glm::value_ptr(projection));
+
+        int object_color_loc = glGetUniformLocation(ball_shaderProgram, "objectColor");
+        glUniform3f(object_color_loc, 0.7f, 0.0f, 0.0f);
+        int light_color_loc = glGetUniformLocation(ball_shaderProgram, "lightColor");
+        glUniform3f(light_color_loc, 1.0f, 1.0f, 1.0f);
+        int light_pos_loc = glGetUniformLocation(ball_shaderProgram, "lightPos");
+        glUniform3f(light_pos_loc, 0.0f, 1.0f, 2.0f);
+        int view_pos_loc = glGetUniformLocation(ball_shaderProgram, "viewPos");
+        glUniform3f(view_pos_loc, 0.0f, 0.0f, 3.0f);
+        
+        glBindVertexArray(VAO_balls);
+        //glBindBuffer(GL_ARRAY_BUFFER, VBO_balls);
+        //glBufferData(GL_ARRAY_BUFFER, sizeof(float) * ball_number * (ball_mesh_resolution_x + 1) * (ball_mesh_resolution_y + 1) * 3, balls_mesh.vertices, GL_STATIC_DRAW);
+        //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        glDrawElements(GL_TRIANGLES, ball_number * 6 * ball_mesh_resolution_x * ball_mesh_resolution_y, GL_UNSIGNED_INT, 0);
+ 
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    glfwTerminate();
+    return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
